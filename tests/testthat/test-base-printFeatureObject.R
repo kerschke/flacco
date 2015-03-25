009d1ae0d438f3fd1deec61c0939eda46c029903
j.d <jan.dagefoerde@googlemail.com>
2015-03-25 13:45:55 +0100

    test output of feature object printing

--- tests/testthat/test-base-printFeatureObject.R
+++ tests/testthat/test-base-printFeatureObject.R
@@ -16,3 +16,31 @@ test_that("FeatureObject Output", {
   
 })
  
+test_that("FeatureObject Output with more than 5 feature dims", {
+  init = matrix(1, ncol=6, nrow=2)
+  colnames(init) = c("a1", "a2", "a3", "a4", "a5", "a6")
+  featobj = createFeatureObject(init, objective = "a6")
+  
+  expect_output(print(featobj), regexp=paste(
+    "^Feature Object:",
+    "- Number of Observations:\\s\\d+",
+    "- Number of Features:\\s\\d+",
+    "- Lower Boundaries: [0-9e+.-]+(, [0-9e+-.]+)*, ...",
+    "- Upper Boundaries: [0-9e+.-]+(, [0-9e+-.]+)*, ...",
+    "- Name of Features: [^,\\s]*(, [^,\\s]*)*",
+    "- Optimization problem: (minimize|maximize) [^\\s]+?",
+    sep="\\r?\\n")
+  )
+  
+})
+
+test_that("FeatureObject Output with a function", {
+  init = matrix(1, ncol=6, nrow=2)
+  colnames(init) = c("a1", "a2", "a3", "a4", "a5", "a6")
+  featobj = createFeatureObject(init, objective = "a6", fun=function (x) sum(x^2))
+  
+  expect_output(print(featobj), regexp=
+    "- Function to be optimized: function \\(x\\) sum\\(x\\^2\\)"
+  )
+  
+})
