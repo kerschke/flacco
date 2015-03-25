d603c569dcd0f52f0d71e05f61359d57635ded72
j.d <jan.dagefoerde@googlemail.com>
2015-03-25 16:20:40 +0100

    more ICoFiS tests; speedup

--- tests/testthat/test-ic-informationContent.R
+++ tests/testthat/test-ic-informationContent.R
@@ -8,8 +8,32 @@ test_that("Information content-based features are computed", {
    lower = -1000, upper = 1000)  
   
   # execution
-  features = calculateInformationContent(featobj)
+  features = calculateInformationContent(featobj, control = 
+           list(ic.epsilon = c(
+             0, 10^(seq(-5,15,length.out=300)) # => fewer epsilons for quicker tests
+           )))
  
+  # postconditions
+  expect_true( testNumber(features$ic.Hmax, FALSE, 0, 1) )
+  # TODO epsilonS
+  expect_true( is.na(features$ic.Mzero) ) 
+  expect_true( is.na(features$ic.epsilon05) ) 
+})
+
+test_that("Information content-based features are computed (Random sorting)", {
+  set.seed(2015*03*25)
+  X = t(replicate(1000, runif(2, -1000, 1000)))
+  y = apply(X, 1, function(x) {x[1]^4 + 1000*(x[1]-3)^3 + 1000*x[1] + x[2]})
+  featobj = createFeatureObject(X = X, y = y, 
+                                lower = -1000, upper = 1000)  
+  
+  # execution
+  features = calculateInformationContent(featobj, control = 
+           list(ic.sorting = "R",
+                ic.epsilon = c(
+                  0, 10^(seq(-5,15,length.out=300)) # => fewer epsilons for quicker tests
+                )))
+  
   # postconditions
   expect_true( testNumber(features$ic.Hmax, FALSE, 0, 1) )
   # TODO epsilonS
@@ -26,9 +50,25 @@ test_that("Information content-based partial information features are computed",
   
   # execution
   features = calculateInformationContent(featobj, control = 
-                                           list(ic.calculate_partial = TRUE))
+           list(ic.calculate_partial = TRUE,
+                ic.epsilon = c(
+                  0, 10^(seq(-5,15,length.out=300)) # => fewer epsilons for quicker tests
+                )))
   
   # postconditions
   expect_true( testNumber(features$ic.Mzero, FALSE, 0, 1) )
   # TODO features$ic.epsilon05
+})
+
+test_that("Information content-based features are not computed for wrong epsilon", {
+  set.seed(2015*03*25)
+  X = t(replicate(1000, runif(2, -1000, 1000)))
+  y = apply(X, 1, function(x) {x[1]^4 + 1000*(x[1]-3)^3 + 1000*x[1] + x[2]})
+  featobj = createFeatureObject(X = X, y = y, 
+                                lower = -1000, upper = 1000)  
+  
+  # execution
+  expect_error( calculateInformationContent(featobj, control = 
+                                           list(ic.epsilon = c(1,2)) )
+  )
 })
