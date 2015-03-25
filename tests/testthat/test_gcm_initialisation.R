59ac7a9bd47388e465f21626dbda85599a056108
j.d <jan.dagefoerde@googlemail.com>
2015-03-25 11:46:15 +0100

    test gcm_init values

--- tests/testthat/test_gcm_initialisation.R
+++ tests/testthat/test_gcm_initialisation.R
@@ -7,7 +7,7 @@ test_that("GCM initialisation for factor-valued objectives is not possible", {
   expect_error( gcm_init(featobj) )
 })
 
-test_that("GCM initialisation computations for min are performed", {
+test_that("GCM initialisation computations for min are performed correctly", {
   values <- iris[, -5] # strip factor column "Species"
   
   # preconditions
@@ -21,6 +21,15 @@ test_that("GCM initialisation computations for min are performed", {
   # postconditions
   expect_equal(length(featobj$env$gcm.representatives$min), 5^3)
   expect_is(featobj$env$gcm.canonicalForm$min, "list")
+  
+  # empty cells
+  expect_equal(length(which(featobj$env$gcm.representatives$min == Inf)), 94)
+  
+  # number of closed classes
+  expect_equal(featobj$env$gcm.canonicalForm$min$closedClassIndex, 92)
+  
+  # TODO values of featobj$env$gcm.canonicalForm$min$indexPermutation
+  # TODO values of featobj$env$gcm.canonicalForm$min$canonicalForm
 })
 
 test_that("GCM initialisation computations for mean are performed", {
@@ -37,9 +46,18 @@ test_that("GCM initialisation computations for mean are performed", {
   # postconditions
   expect_equal(length(featobj$env$gcm.representatives$mean), 5^3)
   expect_is(featobj$env$gcm.canonicalForm$mean, "list")
+  
+  # empty cells
+  expect_equal(length(which(featobj$env$gcm.representatives$mean == Inf)), 94)
+  
+  # number of closed classes
+  expect_equal(featobj$env$gcm.canonicalForm$mean$closedClassIndex, 91)
+  
+  # TODO values of featobj$env$gcm.canonicalForm$mean$indexPermutation
+  # TODO values of featobj$env$gcm.canonicalForm$mean$canonicalForm
 })
 
-test_that("GCM initialisation computations for min are performed", {
+test_that("GCM initialisation computations for near are performed", {
   values <- iris[, -5] # strip factor column "Species"
   
   # preconditions
@@ -53,4 +71,13 @@ test_that("GCM initialisation computations for min are performed", {
   # postconditions
   expect_equal(length(featobj$env$gcm.representatives$near), 5^3)
   expect_is(featobj$env$gcm.canonicalForm$near, "list")
+  
+  # empty cells
+  expect_equal(length(which(featobj$env$gcm.representatives$near == Inf)), 0) # ALWAYS for near!!
+  
+  # number of closed classes
+  expect_equal(featobj$env$gcm.canonicalForm$near$closedClassIndex, 12)
+  
+  # TODO values of featobj$env$gcm.canonicalForm$near$indexPermutation
+  # TODO values of featobj$env$gcm.canonicalForm$near$canonicalForm
 })
