f93444b0b7e3e75f7aea6fe00aaeef47a2574c36
j.d <jan.dagefoerde@googlemail.com>
2015-03-25 16:20:18 +0100

    extracted plotting from ic feature calculation

--- R/ic_feature_information_content.R
+++ R/ic_feature_information_content.R
@@ -199,17 +199,6 @@ calculateInformationContent = function(feat.object, control = list()) {
   # calculate epsilonS [Eq. (6)] ("settling sensitivity")
   epsilonS = log10( epsilon[ min( which(H < 0.05) ) ] )
   
-  if (debug.plot) {
-    plot(log10(epsilon), H, type="l", xlab=expression(log[10](epsilon)))
-    
-    #Hmax
-    points(log10(epsilon[which.max(H)]), Hmax, pch=1)
-    abline(v=log10(epsilon[which.max(H)]), lty = 3)
-    
-    #epsilonS
-    points(epsilonS, 0, pch=0)
-  }
-  
   if (calculate.partial.ic) {
     # calculate Mzero [Eq. (7)] ("initial partial information")
     Mzero = M[1]
@@ -222,49 +211,10 @@ calculateInformationContent = function(feat.object, control = list()) {
       epsilon05 = -Inf
     }
     
-    
-    if (debug.plot) {
-      lines(log10(epsilon), M, lty=2)
-      
-      #Mzero
-      points(log10(epsilon[2]), Mzero, pch=5) # (because log10(0) = -Inf)
-      
-      #epsilon05
-      points(epsilon05, 0, pch=6)
-      abline(v=epsilon05, lty = 3)
-      abline(h=M[ max( which(M > 0.5*Mzero) ) ], lty = 3)
-    } 
   }
   
-  #draw legend
   if (debug.plot) {
-    #lines
-    legend.descr  = c(expression(H(epsilon)))
-    legend.points = c(NA)
-    legend.lines  = c(1)
-    
-    if (calculate.partial.ic) {
-      legend.descr  = c(legend.descr,  expression(M(epsilon)))
-      legend.points = c(legend.points, NA)
-      legend.lines  = c(legend.lines,  2)
-    }
-    
-    #points
-    legend.descr  = c(legend.descr,  expression(H[max]), expression(epsilon[s]))
-    legend.points = c(legend.points, 1, 0)
-    legend.lines  = c(legend.lines,  NA, NA)
-    
-    if (calculate.partial.ic) {
-      legend.descr  = c(legend.descr,  expression(M[0]), expression(epsilon[0.5]))
-      legend.points = c(legend.points, 5, 6)
-      legend.lines  = c(legend.lines,  NA, NA)
-    }
-    
-    
-    legend("topright",
-           legend = legend.descr,
-           pch    = legend.points,
-           lty    = legend.lines)
+    plot_ic (epsilon, calculate.partial.ic, H, M, Hmax, epsilonS, Mzero, epsilon05)
   }
 
   list(
