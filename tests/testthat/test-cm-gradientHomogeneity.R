1cb176c482e22da1350ae25a35aa6a96ac264b5d
j.d <jan.dagefoerde@googlemail.com>
2015-03-26 15:39:41 +0100

    Testing an additional CM scenario

--- tests/testthat/test-cm-gradientHomogeneity.R
+++ tests/testthat/test-cm-gradientHomogeneity.R
@@ -5,11 +5,24 @@ test_that("A cell mapping-enabled FeatureObject is required", {
   expect_error(calculateGradientHomogeneity(featobj))
 })
   
-test_that("GradientHomogeneity Features are calculated", {
+test_that("GradientHomogeneity Features are calculated (differing blocks)", {
   set.seed(2015*03*26)
   X = t(replicate(n = 2000, expr = runif(n = 5, min = -10, max = 10)))
   y = apply(X, 1, function(x) { sum(x^2) })
   featobj = createFeatureObject(X = X, y = y, blocks = c(10, 5, 5, 8, 4))
+  
+  features = calculateGradientHomogeneity(featobj, show.warnings = FALSE)
+  
+  expect_is(features, "list")
+  expect_true( testNumber(features$gradhomo.mean, lower=-1, upper=1) )
+  expect_true( testNumber(features$gradhomo.sd) )
+})
+
+test_that("GradientHomogeneity Features are calculated (identical blocks)", {
+  set.seed(2015*03*26)
+  X = t(replicate(n = 2000, expr = runif(n = 5, min = -10, max = 10)))
+  y = apply(X, 1, function(x) { sum(x^2) })
+  featobj = createFeatureObject(X = X, y = y, blocks = 5)
   
   features = calculateGradientHomogeneity(featobj, show.warnings = FALSE)
   
