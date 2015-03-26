dac1e8e75e4814871fe006d49e476c6ef91ddd90
j.d <jan.dagefoerde@googlemail.com>
2015-03-26 11:29:00 +0100

    testing of IC plots

--- tests/testthat/test-ic-informationContent.R
+++ tests/testthat/test-ic-informationContent.R
@@ -10,7 +10,7 @@ test_that("Information content-based features are computed", {
   # execution
   features = calculateInformationContent(featobj, control = 
            list(ic.epsilon = c(
-             0, 10^(seq(-5,15,length.out=300)) # => fewer epsilons for quicker tests
+             0, 10^(seq(-5,15,length.out=200)) # => fewer epsilons for quicker tests
            )))
  
   # postconditions
@@ -31,7 +31,7 @@ test_that("Information content-based features are computed (Random sorting)", {
   features = calculateInformationContent(featobj, control = 
            list(ic.sorting = "R",
                 ic.epsilon = c(
-                  0, 10^(seq(-5,15,length.out=300)) # => fewer epsilons for quicker tests
+                  0, 10^(seq(-5,15,length.out=200)) # => fewer epsilons for quicker tests
                 )))
   
   # postconditions
@@ -41,7 +41,7 @@ test_that("Information content-based features are computed (Random sorting)", {
   expect_true( is.na(features$ic.epsilon05) ) 
 })
 
-test_that("Information content-based partial information features are computed", {
+test_that("Information content-based features (with partial information) are computed", {
   set.seed(2015*03*25)
   X = t(replicate(1000, runif(2, -1000, 1000)))
   y = apply(X, 1, function(x) {x[1]^4 + 1000*(x[1]-3)^3 + 1000*x[1] + x[2]})
@@ -52,7 +52,7 @@ test_that("Information content-based partial information features are computed",
   features = calculateInformationContent(featobj, control = 
            list(ic.calculate_partial = TRUE,
                 ic.epsilon = c(
-                  0, 10^(seq(-5,15,length.out=300)) # => fewer epsilons for quicker tests
+                  0, 10^(seq(-5,15,length.out=200)) # => fewer epsilons for quicker tests
                 )))
   
   # postconditions
@@ -84,7 +84,7 @@ test_that("Information content-based features are able to compute random samples
   features = calculateInformationContent(featobj, control = 
             list(ic.generate_sample = TRUE,
                  ic.epsilon = c(
-                   0, 10^(seq(-5,15,length.out=300)) # => fewer epsilons for quicker tests
+                   0, 10^(seq(-5,15,length.out=200)) # => fewer epsilons for quicker tests
                  )) )
   
   # postconditions (sadly, samples cannot be tested :( ))
@@ -100,4 +100,45 @@ test_that("Information content-based features are unable to compute samples with
   featobj = createFeatureObject(X = X, y = y)  
   expect_error( calculateInformationContent(featobj, control = 
                                               list(ic.generate_sample = TRUE)) )
+})
+
+test_that("Information content-based features can create a plot", {
+  set.seed(2015*03*25)
+  X = t(replicate(1000, runif(2, -1000, 1000)))
+  y = apply(X, 1, function(x) {x[1]^4 + 1000*(x[1]-3)^3 + 1000*x[1] + x[2]})
+  featobj = createFeatureObject(X = X, y = y, 
+                                lower = -1000, upper = 1000)  
+  
+  # execution
+  features = calculateInformationContent(featobj, control = 
+                                           list(ic.epsilon = c(
+                                             0, 10^(seq(-5,15,length.out=200)) # => fewer epsilons for quicker tests
+                                           ),
+                                           ic.plot = TRUE))
+  
+  # since we cannot capture the created plot properly -- not even using 
+  # evaluate::evaluate --, we can only test whether no errors were raised.
+  
+  expect_true(TRUE) # this can only happen if no error has forced the execution to stop.
+})
+
+test_that("Information content-based features (with partial information) can create a plot", {
+  set.seed(2015*03*25)
+  X = t(replicate(1000, runif(2, -1000, 1000)))
+  y = apply(X, 1, function(x) {x[1]^4 + 1000*(x[1]-3)^3 + 1000*x[1] + x[2]})
+  featobj = createFeatureObject(X = X, y = y, 
+                                lower = -1000, upper = 1000)  
+  
+  # execution
+  features = calculateInformationContent(featobj, control = 
+                                           list(ic.epsilon = c(
+                                             0, 10^(seq(-5,15,length.out=200)) # => fewer epsilons for quicker tests
+                                           ),
+                                           ic.calculate_partial = TRUE,
+                                           ic.plot = TRUE))
+  
+  # since we cannot capture the created plot properly -- not even using 
+  # evaluate::evaluate --, we can only test whether no errors were raised.
+  
+  expect_true(TRUE) # this can only happen if no error has forced the execution to stop.
 })
