bbbb140375013f0551f4fff431f7119caa1dd470
j.d <jan.dagefoerde@googlemail.com>
2015-03-26 15:55:22 +0100

    exclude "misc" test placeholder

--- tests/run-base.R
+++ tests/run-base.R
@@ -4,4 +4,4 @@ test_check("flacco", filter = "^ela")
 test_check("flacco", filter = "^cm")
 test_check("flacco", filter = "^gcm")
 test_check("flacco", filter = "^ic")
-test_check("flacco", filter = "^misc")
+# test_check("flacco", filter = "^misc") # travis complains about this as long as no tests match the filter
