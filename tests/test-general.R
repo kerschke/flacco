3ab5d5d549922a5ed8f9ccf12e14c5b6198a20ab
Pascal Kerschke <pascal.kerschke@uni-muenster.de>
2016-04-04 17:03:00 +0200

    trying to find the correct setting for coveralls

--- tests/test-general.R
+++ tests/test-general.R
@@ -3,6 +3,11 @@ library(testthat)
 # test general stuff:
 test_check("flacco", filter = "^base")
 
+print("TRAVIS:")
+print(Sys.getenv("TRAVIS"))
+print("R_EXPENSIVE_TEST_OK:")
+print(Sys.getenv("R_EXPENSIVE_TEST_OK"))
+
 if (identical(Sys.getenv("TRAVIS"), "true") || identical(Sys.getenv("R_EXPENSIVE_TEST_OK"), "true")) {
   test_check("flacco", filter = "^plot")
 }
