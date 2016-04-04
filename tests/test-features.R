19a25666117259e391994fd84bfc4fb6e0b62ea7
Pascal Kerschke <pascal.kerschke@uni-muenster.de>
2016-04-04 17:22:09 +0200

    trying to run all tests on coverall

--- tests/test-features.R
+++ tests/test-features.R
@@ -1,11 +1,11 @@
 library(testthat)
 
 # test features:
-if (identical(Sys.getenv("TRAVIS"), "true") || identical(Sys.getenv("R_EXPENSIVE_TEST_OK"), "true")) {
+# if (identical(Sys.getenv("TRAVIS"), "true") || identical(Sys.getenv("R_EXPENSIVE_TEST_OK"), "true")) {
   test_check("flacco", filter = "^cm")
   test_check("flacco", filter = "^ela")
   test_check("flacco", filter = "^gcm")
   test_check("flacco", filter = "^ic")
   test_check("flacco", filter = "^misc")
   test_check("flacco", filter = "calculateFeatures")
-}
+# }
--- tests/test-general.R
+++ tests/test-general.R
@@ -3,11 +3,6 @@ library(testthat)
 # test general stuff:
 test_check("flacco", filter = "^base")
 
-print("TRAVIS:")
-print(Sys.getenv("TRAVIS"))
-print("R_EXPENSIVE_TEST_OK:")
-print(Sys.getenv("R_EXPENSIVE_TEST_OK"))
-
-if (identical(Sys.getenv("TRAVIS"), "true") || identical(Sys.getenv("R_EXPENSIVE_TEST_OK"), "true")) {
+# if (identical(Sys.getenv("TRAVIS"), "true") || identical(Sys.getenv("R_EXPENSIVE_TEST_OK"), "true")) {
   test_check("flacco", filter = "^plot")
-}
+# }
