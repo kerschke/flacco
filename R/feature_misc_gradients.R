e271f8f9aa24e1d4ea9518f74868ebfacc585471
kerschke <pascal.kerschke@wi.uni-muenster.de>
2021-09-09 19:56:01 +0200

    add initial version of gradient features (Malan and Engelbrecht, 2013) and hill climbing features (Abell et al, 2013)

---
+++ R/feature_misc_gradients.R
@@ -0,0 +1,49 @@
+calculateLocalSearchFeatures = function(feat.object, control, ...) {
+  assertClass(feat.object, "FeatureObject")
+  if (is.null(feat.object$fun))
+    stop("The gradient features require the exact function!")
+  if (missing(control))
+    control = list()
+  assertList(control)
+  allow.costs = control_parameter(control, "allow_costs", TRUE)
+  if (!allow.costs)
+    stop("You can not prohibit additional costs and still try to compute these features!")
+  measureTime(expression({
+    f = initializeCounter(feat.object$fun)
+    d = feat.object$dim
+    low = feat.object$lower
+    upp = feat.object$upper
+  
+    n.steps = control_parameter(control, "gradient.n_steps", 100L * d)
+    step.size = control_parameter(control, "gradient.step_size", (upp - low) / 20)
+
+    # start by sampling a random point in one of the edges
+    dd = sample(x = c(FALSE, TRUE), size = d, replace = TRUE)
+    x = ifelse(dd, low, upp)
+    y = f(x)
+
+    g.t = NULL
+    for (i in seq_len(n.steps)) {
+      x.t = x
+      y.t = y
+      # sample random direction
+      rd = sample(x = seq_len(d), size = 1L)
+      # avoid moving out of bounds
+      tmp = x[rd] + signs[rd] * step.size[rd]
+      if (!((tmp <= upp[rd]) & (tmp >= low[rd]))) {
+        signs[rd] = -1L * signs[rd]
+      }
+      x[rd] = x[rd] + signs[rd] * step.size[rd]
+      y = f(x)
+      g.t = c(g.t, (y - y.t) / step.size[rd])
+    }
+    
+    G.avg = mean(abs(g.t))
+    G.dev = sqrt(sum((G.avg - abs(g.t))^2) / (n.steps - 1))
+    
+    list(gradient.G_avg = G.avg,
+      gradient.G_dev = G.dev,
+      gradient.costs_fun_evals = showEvals(f)
+    )
+  }), "ela_local")
+}
---
+++ R/feature_misc_hill_climbing.R
@@ -0,0 +1,71 @@
+calculateHillClimbingFeatures = function(feat.object, control, ...) {
+  assertClass(feat.object, "FeatureObject")
+  if (is.null(feat.object$fun))
+    stop("The hill climbing features require the exact function!")
+  allow.costs = control_parameter(control, "allow_costs", TRUE)
+  if (!allow.costs)
+    stop("You can not prohibit additional costs and still try to compute these features!")
+  if (missing(control))
+    control = list()
+  assertList(control)
+  measureTime(expression({
+    f = initializeCounter(feat.object$fun)
+    d = feat.object$dim
+    N = control_parameter(control, "hill_climbing.local_searches", 100L)
+    budget_per_run = control_parameter(control, "hill_climbing.max_iterations_per_run", 1000L * d)
+    opt.algo = control_parameter(control, "hill_climbing.optim_method", "L-BFGS-B")
+    d.meth = control_parameter(control, "hill_climbing.dist_method", "minkowski")
+    d.p = control_parameter(control, "hill_climbing.dist_p", 2L)
+    ## TODO: find a way to force L-BFGS-B to stop after a fixed number of fun evals (not iterations)
+    opt.algo.control = control_parameter(control, "hill_climbing.optim_method_control", list(maxit = budget_per_run, ...))
+    low = control_parameter(control, "hill_climbing.optim.lower", if (opt.algo == "L-BFGS-B") feat.object$lower else -Inf)
+    upp = control_parameter(control, "hill_climbing.optim.upper", if (opt.algo == "L-BFGS-B") feat.object$upper else Inf)
+    opt.algo.control$fnscale = ifelse(feat.object$minimize, 1L, -1L)
+    id.seed = control_parameter(control, "hill_climbing.sample_seed", sample(seq_len(1e6), 1L))
+
+    ## helper function for running the local searches and returning the relevant information
+    calcOptim = function(par, ...) {
+      res = optim(as.numeric(par), fn, method = opt.algo, control = opt.algo.control, lower = low, upper = upp, ...)
+      return(list(par = res$par, fun.val = res$value, counts = resetCounter(fn)))
+    }
+
+    ## sample local search starting positions
+    set.seed(id.seed)
+    ls.starts = vapply(seq_len(d), function(i) runif(N, min = low[i], max = upp[i]), double(N))
+
+    fn = initializeCounter(f)
+    result = lapply(seq_len(N), function(i) calcOptim(ls.starts[i,], ...))
+    pars = t(vapply(result, function(res) res$par, double(d)))
+    fun.values = vapply(result, function(res) res$fun.val, double(1L))
+    fun.evals = vapply(result, function(res) res$counts, integer(1L))
+
+    dists = dist(pars, method = d.meth, p = d.p)
+    dists = as.vector(dists)
+
+    ## which (local) optima are globally optimal?
+    if (feat.object$minimize) {
+      global.opt = which(fun.values == min(fun.values))
+    } else {
+      global.opt = which(fun.values == max(fun.values))
+    }
+
+    d.rel = vapply(global.opt, function(g.opt) {
+      j = seq_len(g.opt - 1L)
+      d.rel = c(dists[cumsum(N - j) - (N - g.opt)], 0)
+      if (g.opt < N) {
+        d.rel = c(d.rel, dists[(sum(N - j) + 1L) : sum(N - seq_len(g.opt))])
+      }
+      return(d.rel)
+    }, double(N))
+
+    list(
+      hill_climbing.dist_between_opt.mean = mean(dists),
+      hill_climbing.dist_between_opt.sd = sd(dists),
+      hill_climbing.dist_local_to_global.mean = mean(d.rel),
+      hill_climbing.dist_local_to_global.sd = sd(d.rel),
+      fun.evals = fun.evals,
+      budget.per.run = ls.budget_per_run,
+      hill_climbing.costs_fun_evals = sum(fun.evals)
+    )
+  }), "hill_climbing")
+}
